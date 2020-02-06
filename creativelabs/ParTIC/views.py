89d53448ebfb17a9f052a42c316c3e45242e2cd4
Rohan Battula <rohanbattula@gmail.com>
2020-02-05 20:39:41 -0800

    created initial serializer and models

--- creativelabs/ParTIC/views.py
+++ creativelabs/ParTIC/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from models import Party
+from serializer import PartySerializer
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
