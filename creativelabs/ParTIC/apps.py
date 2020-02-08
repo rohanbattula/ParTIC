9211cd6d438bd641e7c73ab1b0a038cc36cebc9d
Parth Manoj Deshpande <Parth@s-169-232-87-242.resnet.ucla.edu>
2020-02-08 15:13:57 -0800

    deleted

--- creativelabs/ParTIC/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParticConfig(AppConfig):
-    name = 'ParTIC'
--- creativelabs/ParTIC/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from models import Party
-
-class PartySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Party
-        fields = ('name', 'address','status','distance',
-        'entryFee','dateTime','guysAllowed')
