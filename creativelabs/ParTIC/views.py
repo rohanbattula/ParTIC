0ed175026fbf43ac9628b7a83601f2e7fe57facc
Parth Manoj Deshpande <Parth@s-169-232-87-242.resnet.ucla.edu>
2020-02-08 14:49:03 -0800

    preliminary views done

--- creativelabs/ParTIC/views.py
+++ creativelabs/ParTIC/views.py
@@ -4,3 +4,19 @@ from models import Party
 from serializer import PartySerializer
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+def index(request):
+    rest_list = Party.objects.order_by('-dateTime')
+    context = {'rest_list': rest_list}
+    return render(request, 'index/index.html', context)
+
+
+# Rest api end point
+def get_rest_list(request):
+    """
+    Returns Json list of all restaurants
+    """
+    if request.method == "GET":
+        rest_list = Party.objects.order_by('-dateTime')
+        serializer = PartySerializer(rest_list, many=True)
+        return JsonResponse(serializer.data, safe=False)
