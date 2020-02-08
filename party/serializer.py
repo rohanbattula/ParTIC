ff97157c87a56a82e9a4189ad3681626ab9bdaeb
Rohan Battula <rohanbattula@gmail.com>
2020-02-08 15:52:28 -0800

    Creating new copy of repo

---
+++ party/serializer.py
@@ -0,0 +1,10 @@
+'''
+from rest_framework import serializers
+from models import Party
+
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Party
+        fields = ('name', 'address','status','distance',
+        'entryFee','datetime','guysAllowed')
+'''
