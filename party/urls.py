78b0b5d30b8661cb23a996e30ad003addb8d3906
Rohan Battula <rohanbattula@gmail.com>
2020-02-24 16:34:32 -0800

    merging

--- party/urls.py
+++ party/urls.py
@@ -6,9 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
-=======
     #path('admin/', admin.site.urls),
->>>>>>> 9e007c8d4879154fa94aaaaec53cd6b34a9f382d
     path('api/list', views.get_rest_list, name='get_rest_list'),
 ]
